b81f675af3e17a9589cda179108843d90b9943c8
= <=>
2020-03-28 13:40:00 -0500

    update metrics and generate graphs

--- index.js
+++ index.js
@@ -1,41 +1,38 @@
 const exec = require('child_process').exec;
-const fs = require('fs');
-const apiUrl = 'http://<api-url>:<port>';
+const CliProgress = require('cli-progress');
+const {getMetrics} = require('./lib/processor');
+const graphAll = require('./lib/metrics');
+const {createFile, closeFile, writeMetrics, createLuaDefinition} = require('./lib/file-utils');
+const progress = new CliProgress.SingleBar({}, CliProgress.Presets.shades_classic);
 
 const duration = '3s'; // Tiempo de duración de cada request de medición
 const threads = 12; // Cantidad de hilos utilizados por Wrk
-const iterations = 1500; // Cantidad de iteraciones de la medición
+const iterations = 15; // Cantidad de iteraciones de la medición
 const initialIter = 1; // Punto de inicio de la medición
+const connIncrement = 7; // incremento de conexiones
 
-/**
- * Se encarga de escribir resultados en un archivo de texto
- * @param {string} result resultado a registrar
- *
- */
-function writeResult(result) {
-  fs.appendFile('result.txt', result, err => {
-    if (err) console.log(err);
-  });
-}
+
+const apiUrl = 'https://reqres.in/api/users?page=2&per_page=1';
+const method = 'POST';
+const body = '{"properties":{"age":19}}';
+const type = 'application/json';
 
 /**
  * Ejecuta un comando enviado por parámetro
  * @param {string} cmd comando a ejecutar
  */
 function execShellCommand(cmd) {
-  return new Promise((resolve, reject) => {
-    exec(cmd, (error, stdout, stderr) => {
-      if (error) reject(error);
-      try {
-        let map = stdout.split(':');
-        let tps = map[1];
-        resolve(tps.trim());
-      } catch (error) {
-        console.log(error);
-        resolve(0);
-      }
+    return new Promise((resolve, reject) => {
+        exec(cmd, (error, stdout) => {
+            if (error) reject(error);
+            try {
+                resolve(getMetrics(stdout));
+            } catch (error) {
+                console.log(error);
+                resolve({tps: 0, tpsAvg: 0, latencyAvg: 0});
+            }
+        });
     });
-  });
 }
 
 /**
@@ -43,15 +40,26 @@ function execShellCommand(cmd) {
  * para la ejecución de cada medición.
  */
 async function startBenchmark() {
-  for (let conn = initialIter; conn <= iterations; conn += 7) {
-    let realThreads = conn < threads ? conn : threads;
+    createLuaDefinition(method, body, type);
+    createFile();
+    progress.start(iterations, 0);
+    for (let conn = initialIter; conn <= iterations; conn += connIncrement) {
+        let realThreads = conn < threads ? conn : threads;
 
-    let cmd = `wrk -t${realThreads} -c${conn} -d${duration} ${apiUrl} | grep Requests`;
-    await execShellCommand(cmd).then(value => {
-      writeResult(conn + ',' + value + '\n');
-      console.log(`${conn},${value}\n`);
-    });
-  }
+        let cmd = `wrk -t${realThreads} -c${conn} -d${duration} ${apiUrl}`;
+        await execShellCommand(cmd).then(metrics => {
+            metrics.conn = conn;
+            writeMetrics(metrics);
+        });
+        progress.update(conn);
+    }
+    closeFile();
+    progress.stop();
+    await graphAll();
 }
 
-startBenchmark();
+startBenchmark()
+    .then(() => {
+        console.log('END');
+    })
+    .catch(err => console.log(err));
