f7be5f2dc3a4b39f53328f6d90c1a5e0421fed57
= <=>
2020-03-29 09:15:05 -0500

    update lua params, extrac params as json file

--- index.js
+++ index.js
@@ -2,29 +2,27 @@
 
 const {startBenchmark} = require('./lib/benchmark');
 
-console.log(process.argv);
+// let options = {
+//     request: {
+//         apiUrl: '<protocol>://<hostname>:<port>/<path>', // endpoint
+//         method: 'POST', // Http Method
+//         body: '{"properties":{"age":19}}', // Request body
+//         headers: [{name: 'Content-Type', value: 'application/json'}] // Headers
+//     },
+//     benchmark: {
+//         duration: '3s', // Tiempo de duración de cada request de medición
+//         threads: 12, // Cantidad de hilos utilizados por Wrk
+//         iterations: 50, // Cantidad de iteraciones de la medición
+//         initialIter: 1, // Punto de inicio de la medición
+//         connIncrement: 7 // incremento de conexiones
+//     }
+// };
 
-let options = {
-    request: {
-        apiUrl: 'http://localhost:8082/client/apps/app/features/feature',
-        method: 'POST',
-        body: '{"properties":{"age":19}}',
-        type: 'application/json'
-    },
-    benchmark: {
-        duration: '3s', // Tiempo de duración de cada request de medición
-        threads: 12, // Cantidad de hilos utilizados por Wrk
-        iterations: 50, // Cantidad de iteraciones de la medición
-        initialIter: 1, // Punto de inicio de la medición
-        connIncrement: 7 // incremento de conexiones
-    }
-};
-
-console.log(JSON.stringify(options, null, 4));
-
-if (process.argv[2]) {
-    options = require(process.argv[2]);
+if (!process.argv[2]) {
+    return console.log('Usage: npm start options.json');
 }
+
+const options = require(`${__dirname}/${process.argv[2]}`);
 
 startBenchmark(options)
     .then(() => {
